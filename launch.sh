b2ca7ed54983b56f383f23a7c472853ec638909c
Francisco Pinto Santos <franpintosantos@usal.es>
2021-08-26 09:11:13 +0200

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Copyright 2021 Deep Trends
 # See LICENSE for details.
+
 sudo python3 -m pip install --upgrade pip
 sudo python3 -m pip install . --upgrade
 sudo python3 -m pip install --upgrade git+https://github.com/twintproject/twint.git@origin/master#egg=twint
